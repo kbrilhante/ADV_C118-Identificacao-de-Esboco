b55534d29c741a288172671ae0f0d74465f040ba
Karina <kbrilhante@gmail.com>
2023-01-19 20:05:22 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const corDeFundo = "#fff";
+
+function preload() {}
+
+function setup() {
+    var canvas = createCanvas(250, 250);
+    canvas.center();
+    background(corDeFundo);
+}
+
+function draw() {}
+
+function limpaTela() {
+    background(corDeFundo);
+}
